a2a36c3417faa8990d4b9d9f35053124cc65e240
Robbilie <github@eneticum.de>
2016-02-03 18:09:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 
 	Widget.initialize({
 		title: "Search",
-		tabs: ["Search"],
 		width: 300,
 		height: 400,
 	}, widget => {
@@ -11,11 +10,17 @@
 		// debugging
 		console.log("widget", widget);
 
-		// create search tab
-		widget.setTabContent("Search",
-			eowEl("webview", { 
-				src: "https://playground.eneticum.de/search/" 
-			})
-			.on("dom-ready", function () { this.insertCSS("body, select { background-color: #131313; color: white; }"); })
-		);
+		var tabs = eowTabs("div", {}, [{
+			name: "Search",
+			content: [
+				eowEl("webview", { 
+					src: "https://playground.eneticum.de/search/" 
+				})
+				.on("dom-ready", function () { this.insertCSS("body, select { background-color: #131313; color: white; }"); })
+			]
+		}]);
+		
+		widget.appendChild(tabs);
+		
+		tabs.selectTab("Search");
 	});
